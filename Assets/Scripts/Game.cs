2aa652e49209034188e4846d2481c4c2a06da3a3
VoidsArchive <snomonchrom@gmail.com>
2026-04-02 15:38:39 -0700

    Game Over Screen added

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,16 +1,41 @@
+using System;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
     public UI Ui;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameTimer GameTimer;
+    
     void Start()
     {
-        
+        Ui.HideGameOverScreen();
+        Ui.ShowStartScreen();
     }
 
     public void OnStartButtonClicked()
     {
         Ui.HideStartScreen();
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        GameTimer.StartTimer(10, OnTimerFinished);
+    }
+
+    private void OnTimerFinished()
+    {
+        Ui.ShowGameOverScreen();
+    }
+
+    private void Update()
+    {
+        Ui.ShowTime();
+    }
+
+    public void OnPlayAgainButtonClicked()
+    {
+        Ui.HideGameOverScreen();
+        StartGame();
     }
 }
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,6 +5,9 @@ public class UI : MonoBehaviour
 {
     public TMP_Text scoreText;
     public CanvasGroup StartScreenCanvasGroup;
+    public CanvasGroup GameOverScreenCanvasGroup;
+    public TMP_Text TimeText;
+    public GameTimer GameTimer;
     public void SetScoreText(int score)
     {
         scoreText.text = "Score: " + score;
@@ -13,5 +16,25 @@ public class UI : MonoBehaviour
     public void HideStartScreen()
     {
         CanvasGroupDisplayer.Hide(StartScreenCanvasGroup);
+    }
+
+    public void ShowStartScreen()
+    {
+        CanvasGroupDisplayer.Show(StartScreenCanvasGroup);
+    }
+
+    public void ShowTime()
+    {
+        TimeText.text = GameTimer.GetTimeAsString();
+    }
+
+    public void HideGameOverScreen()
+    {
+        CanvasGroupDisplayer.Hide(GameOverScreenCanvasGroup);
+    }
+
+    public void ShowGameOverScreen()
+    {
+        CanvasGroupDisplayer.Show(GameOverScreenCanvasGroup);
     }
 }
